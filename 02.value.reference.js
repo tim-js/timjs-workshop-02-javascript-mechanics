912801b7870886dedc70bd8fa62d3bc242027af2
Andrei Pfeiffer <andrei@upsidedown.ro>
2016-07-13 15:07:23 +0300

    Cleanup final code

--- 02.value.reference.js
+++ 02.value.reference.js
@@ -2,111 +2,32 @@
  *  Values
  */
 
-// primitives are values
-var v1 = 'a';
-var v2 = v1;
-// console.log(v1, v2);
-
-v2 = 'b';
-// console.log(v1, v2);
-// console.log(v1 === v2);
-
 
 /**
  *  References
  */
-
-// objects are references
-var o1 = { a: 1 };
-var o2 = o1;
-// console.log(o1, o2);
-
-o2.b = 2;
-// console.log(o1, o2);
-// console.log(o1 === o2);
-
-// how do you think should be "normal"?
-// "2 red cars"
 
 
 /**
  *  Passing by Value vs. Passing by Reference
  */
 
-function inc(nr) {
-	nr += 1;
-	return nr;
-}
-var n = 2;
-// console.log(inc(n), n);
-
-function incObj(obj) {
-	obj.nr += 1;
-	return obj;
-}
-var o = { nr: 2 };
-// console.log(incObj(o), o);
-// console.log(inc(o.nr), o.nr);
-
 
 /**
  *  Equality vs. Deep Equality
  */
-
-// console.log( 1 === 1 );
-// console.log( 'a' === 'a' );
-// console.log( {} === {} );
-
-// objects are "equal" if they have the same reference
-// objects are "deep equal" if they have the same "content"
-
-// QUIZ: implement deepEqual()
-
-function isDeepEqual(v1, v2) {
-
-}
-
-isDeepEqual(1, 1); // true
-isDeepEqual(1, 2); // false
-isDeepEqual({ a: 1 }, { a: 1 }); // true
-isDeepEqual({ a: 1 }, { b: 1 }); // false
 
 
 /**
  *  Object Cloning
  */
 
-var o1 = { a: 1 };
-var o2 = clone(o1);
-
-// QUIZ: implement clone()
-
-function clone(obj) {
-
-}
-
-// console.log( o1 === o2 ); // false
-// console.log( isDeepEqual(o1, o2) ); // true
-
 
 /**
  *  Impure Functions
  */
 
-var n = 1;
-var impure1 = () => n += 1;
-console.log(n, impure1(n), n);
-
-var impure2 = (obj) => obj.nr += 1;
-console.log(o.nr, impure2(o), o.nr);
-
 
 /**
  *  Pure Functions
  */
-
-var pure1 = (nr) => nr += 1;
-console.log(n, pure1(n), n);
-
-var pure2 = (obj) => obj.nr + 1;
-console.log(o.nr, pure2(o), o.nr);
--- 04.arguments.js
+++ 04.arguments.js
@@ -2,59 +2,17 @@
  *  Arguments
  */
 
-function a(x) {
-	console.log(x);
-}
-// a(1);
-// a(1, 2);
-// a();
-
 
 /**
  *  Default values
  */
-
-function double(arr) {
-	arr = (typeof arr === 'undefined') ? [] : arr;
-	arr = arr || [];
-	return arr.map(i => i + i);
-}
-
-function double(arr = []) {
-	return arr.map(i => i + i);
-}
-
-// console.log( double([1, 2]) );
-// console.log( double() );
 
 
 /**
  *  Arguments object
  */
 
-function b() {
-	console.log(arguments);
-}
-b();
-b(1);
-b(1, 2);
-
-// contains all passed arguments
-// has a length property
-// "array-like" object
-// but not an actual array
-
 
 /**
  *  Dynamic arguments
  */
-
-function sum() {
-	// const arr = [].slice.call(arguments);
-	// const arr = Array.from(arguments);
-}
-
-sum(); // 0
-sum(1); // 1
-sum(1, 2); // 3
-sum(1, 2, 3, 4, 5); // 15
